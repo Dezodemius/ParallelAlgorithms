df757fdc9a52f055ff72818f3af0f3ce1c8c792b
Yehor Hladkov <gladkovyegor@gmail.com>
2020-04-16 00:08:00 +0400

    Создал бенчмарки для тестирования скалярныйх произведений.
    - Реализовал скалярное произведение на float и double.
    - Реализовал скалярное произведение с развёртыванием циклов с шагами 2 и 4.
    - Создал скрипт для запуска.

---
+++ run.sh
@@ -0,0 +1,24 @@
+echo "COMPILING"
+mkdir bin
+if ! gcc main.c -o bin/main.out
+then
+  exit
+fi
+
+echo "DONE"
+
+echo "WITHOUT OPTIMIZATION RUN"
+./bin/main.out -O0
+echo "DONE"
+
+echo "-O1 OPTIMIZATION RUN"
+./bin/main.out -O1
+echo "DONE"
+
+echo "-O2 OPTIMIZATION RUN"
+./bin/main.out -O2
+echo "DONE"
+
+echo "-O3 OPTIMIZATION RUN"
+./bin/main.out -O3
+echo "DONE"
